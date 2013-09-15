b6682729b2d7510ba7ce1f24cc761c477ca68ba3
Dylan Howey <howeyman09@gmail.com>
2013-09-15 16:19:09 -0500

    decreased timestep. calculation is written to "output.txt"

--- runge.c
+++ runge.c
@@ -9,9 +9,9 @@ static const float ALPHA = 1;
 static const float GAMMA = 1;
 static const float HX = 0;
 static const float HY = 0;
-static const float HZ = 100;
+static const float HZ = 10;
 static const float M;
-static const float TIMESTEP = 1000;
+static const float TIMESTEP = .005;
 
 float phiDot(float theta, float phi) {
 	
@@ -39,17 +39,17 @@ float k4theta(float theta) {
 }
 
 int main() {
-	#if 0
 	output = fopen("output.txt", "w");
 	if(output == NULL) {
 		printf("error opening file\n");
 		return 0;
 	}
-	#endif
 	float t = 0;
 	float theta = 1;
-	for(t = 0; t < 100000000; t += TIMESTEP) {
+	fprintf(output, "theta: %f\tt: %f\n", theta, t);
+	for(t = 0; t < 2; t += TIMESTEP) {
 		theta = theta + (1.0/6.0) * (k1theta(theta) + 2.0 * k2theta(theta) + 2.0 * k3theta(theta) + k4theta(theta)) * TIMESTEP;
-		printf("theta: %f\tt: %f\n", theta, t);
+		printf("theta: %f\tt: %f\n", theta, t + TIMESTEP);
+		fprintf(output, "theta: %f\tt: %f\n", theta, t + TIMESTEP);
 	}
 }	
