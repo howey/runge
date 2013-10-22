d44bb76cbd5cd56e3fae178c16a220bff2c3d65b
Dylan Howey <howeyman09@gmail.com>
2013-10-22 11:59:17 -0500

    added thermal motion. made marsaglia a submodule

--- runge.c
+++ runge.c
@@ -4,32 +4,33 @@
 #include <stdlib.h>
 #include <math.h>
 #include <stdbool.h>
+#include "marsaglia-random/mars.h"
 
 static FILE * output;
 
-static const float ALPHA = 35186; //alpha*gamma / (1 + alpha^2)
-static const float GAMMA = 1.76e7;
-//static const float M = 1000;
-static const float TIMESTEP = .0000000005;
-static const float K = 1e6;
+static const double ALPHA = 35186; //alpha*gamma / (1 + alpha^2)
+static const double GAMMA = 1.76e7;
+//static const double M = 1000;
+static const double TIMESTEP = .00000000005;
+static const double K = 1e6;
 
 typedef struct {
-	float x;
-	float y;
-	float z;
+	double x;
+	double y;
+	double z;
 } Vector;
 
 typedef struct {
-	float r;
-	float theta;
-	float phi;
+	double r;
+	double theta;
+	double phi;
 } SphVector;
 
 //Computes the anisotropy field and writes the result to a Vector H
 void anisotropyH(Vector * H, const SphVector * M) {
-	H->x = (1/M->r) * -2 * K * cosf(M->theta) * sinf(M->theta) * cosf(M->phi) * cosf(M->theta);
-	H->y = (1/M->r) * -2 * K * cosf(M->theta) * sinf(M->theta) * sinf(M->phi) * cosf(M->theta);
-	H->z = (1/M->r) * 2 * K * cosf(M->theta) * powf(sinf(M->theta), 2);
+	H->x = (1/M->r) * -2 * K * cos(M->theta) * sin(M->theta) * cos(M->phi) * cos(M->theta);
+	H->y = (1/M->r) * -2 * K * cos(M->theta) * sin(M->theta) * sin(M->phi) * cos(M->theta);
+	H->z = (1/M->r) * 2 * K * cos(M->theta) * pow(sin(M->theta), 2);
 }
 
 //Adds rectangular vectors A and B and stores the results in A
@@ -39,69 +40,68 @@ void addVector(Vector * A, const Vector * B) {
 	(A->z) += (B->z);
 }
 
-float phiDot(const float theta, const float phi, const Vector * H) {
-	return GAMMA * ((cosf(theta) * sinf(phi) * H->y) / sinf(theta) + (cosf(theta) * cosf(phi) * H->x) / sinf(theta) - H->z) + ALPHA * ((cosf(phi) * H->y) / sinf(theta) - (sinf(phi) * H->x) / sin(theta));	
+double phiDot(const double theta, const double phi, const Vector * H) {
+	return GAMMA * ((cos(theta) * sin(phi) * H->y) / sin(theta) + (cos(theta) * cos(phi) * H->x) / sin(theta) - H->z) + ALPHA * ((cos(phi) * H->y) / sin(theta) - (sin(phi) * H->x) / sin(theta));	
 }
 
-float thetaDot(const float theta, const float phi, const Vector * H) {
-	return -GAMMA * (cosf(phi) * H->y - sinf(phi) * H->x)\
-	+ ALPHA * (cosf(theta) * cosf(phi) * H->x - H->z * sinf(theta) + cosf(theta) * sinf(phi) * H->y);
+double thetaDot(const double theta, const double phi, const Vector * H) {
+	return -GAMMA * (cos(phi) * H->y - sin(phi) * H->x)\
+	+ ALPHA * (cos(theta) * cos(phi) * H->x - H->z * sin(theta) + cos(theta) * sin(phi) * H->y);
 }
 
 // Terms for RK4
-float k1theta(const float theta, const Vector * H) {
+double k1theta(const double theta, const Vector * H) {
 	return thetaDot(theta, 0, H);
 }
 
-float k2theta(const float theta, const Vector * H) {
+double k2theta(const double theta, const Vector * H) {
 	return thetaDot(theta + 0.5 * k1theta(theta, H) * TIMESTEP, 0, H);
 }
 
-float k3theta(const float theta, const Vector * H) {
+double k3theta(const double theta, const Vector * H) {
 	return thetaDot(theta + .5 * k2theta(theta, H) * TIMESTEP, 0, H);
 }
 
-float k4theta(const float theta, const Vector * H) {
+double k4theta(const double theta, const Vector * H) {
 	return thetaDot(theta + k3theta(theta, H) * TIMESTEP, 0, H);
 }
 
-float k1phi(const float phi, const Vector * H) {
+double k1phi(const double phi, const Vector * H) {
 	return phiDot(1, phi, H);
 }
 
-float k2phi(const float phi, const Vector * H) {
+double k2phi(const double phi, const Vector * H) {
 	return phiDot(1, phi+ 0.5 * k1phi(phi, H) * TIMESTEP, H);
 }
 
-float k3phi(const float phi, const Vector * H) {
+double k3phi(const double phi, const Vector * H) {
 	return phiDot(1, phi + .5 * k2phi(phi, H) * TIMESTEP, H);
 }
 
-float k4phi(const float phi, const Vector * H) {
+double k4phi(const double phi, const Vector * H) {
 	return phiDot(1, phi + k3phi(phi, H) * TIMESTEP, H);
 }
 
-void simulate(const Vector * H, SphVector * M, const float endTime) {
-	for(float t = 0; t < endTime; t += TIMESTEP) {
+void simulate(const Vector * H, SphVector * M, const double endTime) {
+	for(double t = 0; t < endTime; t += TIMESTEP) {
 		M->theta = M->theta + (1.0/6.0) * (k1theta(M->theta, H) + 2.0 * k2theta(M->theta, H) + 2.0 * k3theta(M->theta, H) + k4theta(M->theta, H)) * TIMESTEP;
 	}
 	
-	for(float t = 0; t < endTime; t += TIMESTEP) {
+	for(double t = 0; t < endTime; t += TIMESTEP) {
 		M->phi = M->phi + (1.0/6.0) * (k1phi(M->phi, H) + 2.0 * k2phi(M->phi, H) + 2.0 * k3phi(M->phi, H) + k4phi(M->phi, H)) * TIMESTEP;
 	}
 }
 
 int main(int argc, char *argv[]) {
-	//float theta = 1/57.3; //57.3 degrees in a radian
-	//float phi = 0;
-	float endTime;
-	float time;
+	double endTime;
+	double time;
+	double sd;
 
 	//The applied field, H
 	Vector * applH = malloc(sizeof(Vector));
-	applH->x = 0.0f;
-	applH->y = 0.0f;
-	applH->z = 2500.0f;
+	applH->x = 0.0;
+	applH->y = 0.0;
+	applH->z = 2500.0;
 
 	//The magnetization, M
 	SphVector * M = malloc(sizeof(SphVector));
@@ -111,15 +111,15 @@ int main(int argc, char *argv[]) {
 	
 	//The anisotropy field
 	Vector * anisH = malloc(sizeof(Vector));
-	anisH->x = 0.0f;
-	anisH->y = 0.0f;
-	anisH->z = 0.0f;
+	anisH->x = 0.0;
+	anisH->y = 0.0;
+	anisH->z = 0.0;
 
 	//The effective field
 	Vector * effH = malloc(sizeof(Vector));
-	effH->x = 0.0f;
-	effH->y = 0.0f;
-	effH->z = 0.0f;
+	effH->x = 0.0;
+	effH->y = 0.0;
+	effH->z = 0.0;
 
 	output = fopen("output.txt", "w");
 	if(output == NULL) {
@@ -135,30 +135,38 @@ int main(int argc, char *argv[]) {
 	else 
 		endTime = strtof(argv[1], NULL);
 	
+	sd = (3.45e-4)/sqrt(endTime);
 	bool isDecreasing = true;
 	do {
 		//anisotropyH(anisH, M);	
 
-		effH->x = 0.0f;
-		effH->y = 0.0f;
-		effH->z = 0.0f;
+		effH->x = 0.0;
+		effH->y = 0.0;
+		effH->z = 0.0;
+
+		effH->x += gaussian(0, sd);
+		effH->y += gaussian(0, sd);
+		effH->z += gaussian(0, sd);
 
 		//addVector(effH, anisH);
 		addVector(effH, applH);
 		
 		simulate(effH, M, endTime);
-		if(applH->z + 2500.0f < 1.0f)
+		if(applH->z + 2500.0 < 1.0)
 			isDecreasing = false;
-		isDecreasing ? (applH->z -= 50.0f) : (applH->z += 50.0f);
-		//fprintf(output, "M(%e) = %fx + %fy + %fz\n", time, M->r * sinf(M->theta) * cosf(M->phi), M->r * sinf(M->theta) * sinf(M->phi), M->r * cosf(M->theta));
-		fprintf(output, "%e\t%f\n", applH->z, M->r * cosf(M->theta));
+		isDecreasing ? (applH->z -= 50.0) : (applH->z += 50.0);
+		//fprintf(output, "M(%e) = %fx + %fy + %fz\n", time, M->r * sin(M->theta) * cos(M->phi), M->r * sin(M->theta) * sin(M->phi), M->r * cos(M->theta));
+		fprintf(output, "%e\t%f\n", applH->z, M->r * cos(M->theta));
 		time += endTime;
-	} while(applH->z - 2500.0f < 1.0f);
+	} while(applH->z - 2500.0 < 1.0);
 
-	printf("M(%e) = %fx + %fy + %fz\n", endTime, M->r * sinf(M->theta) * cosf(M->phi), M->r * sinf(M->theta) * sinf(M->phi), M->r * cosf(M->theta));
+	printf("M(%e) = %fx + %fy + %fz\n", endTime, M->r * sin(M->theta) * cos(M->phi), M->r * sin(M->theta) * sin(M->phi), M->r * cos(M->theta));
 	
 	fclose(output);
+	
 	free(applH);
+	free(effH);
+	free(anisH);
 	free(M);
 	return 0;
 }	
