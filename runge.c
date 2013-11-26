49574fa3660de24d1f0a9fefb57a0a5a16f68873
Dylan Howey <howey014@umn.edu>
2013-11-26 15:48:39 -0600

    fixed value of alpha.

--- runge.c
+++ runge.c
@@ -6,11 +6,14 @@
 #include "runge.h"
 #include "mars.h"
 
-/* Time is in units of us */
-static const double ALPHA = 0.035186; //alpha*gamma / (1 + alpha^2)
-static const double GAMMA = 1.76e1;
+#define USE_THERMAL 1 
+
+/* Time is in units of ns */
+//TODO: Compute alpha here, not on your calculator
+static const double ALPHA = 0.02; 
+static const double GAMMA = 1.76e-2;
 static const double K = 1e6;
-static const double TIMESTEP = (1e-8);	
+static const double TIMESTEP = (1e-5);	
 
 static double *xx;
 static SphVector **y;
@@ -26,8 +29,8 @@ void anisotropyH(Vector * Ha, const SphVector * M) {
 void mDot(double t, SphVector M[], SphVector dMdt[], int n) {
 	for(int i = 0; i < n; i++) {
 		dMdt[i].r = 0;
-		dMdt[i].phi = GAMMA * ((cos(M[i].theta) * sin(M[i].phi) * H.y) / sin(M[i].theta) + (cos(M[i].theta) * cos(M[i].phi) * H.x) / sin(M[i].theta) - H.z) + ALPHA * ((cos(M[i].phi) * H.y) / sin(M[i].theta) - (sin(M[i].phi) * H.x) / sin(M[i].theta));
-		dMdt[i].theta = -GAMMA * (cos(M[i].phi) * H.y - sin(M[i].phi) * H.x) + ALPHA * (cos(M[i].theta) * cos(M[i].phi) * H.x - H.z * sin(M[i].theta) + cos(M[i].theta) * sin(M[i].phi) * H.y);
+		dMdt[i].phi = GAMMA * ((cos(M[i].theta) * sin(M[i].phi) * H.y) / sin(M[i].theta) + (cos(M[i].theta) * cos(M[i].phi) * H.x) / sin(M[i].theta) - H.z) + ((ALPHA * GAMMA)/(1 + ALPHA * ALPHA)) * ((cos(M[i].phi) * H.y) / sin(M[i].theta) - (sin(M[i].phi) * H.x) / sin(M[i].theta));
+		dMdt[i].theta = -GAMMA * (cos(M[i].phi) * H.y - sin(M[i].phi) * H.x) + ((ALPHA * GAMMA)/(1 + ALPHA * ALPHA)) * (cos(M[i].theta) * cos(M[i].phi) * H.x - H.z * sin(M[i].theta) + cos(M[i].theta) * sin(M[i].phi) * H.y);
 	}
 }
 
@@ -55,13 +58,15 @@ void rkdumb(SphVector vstart[], int nvar, double x1, double x2, int nstep, void 
 	x = x1;
 	h = (x2-x1)/nstep;
 
-	double sd = 0.34/sqrt(TIMESTEP);
+	double sd = 3.4e-4/sqrt(TIMESTEP * 1e-9);
 	for (int k = 0; k < nstep; k++) {
 
 		// Add in thermal motion
+		#if USE_THERMAL 
 		double thermX = gaussian(0, sd);
 		double thermY = gaussian(0, sd);
 		double thermZ = gaussian(0, sd);
+		#endif
 
 		//Add in anisotropy
 		anisotropyH(&Hanis, &y[0][k]);
@@ -69,9 +74,11 @@ void rkdumb(SphVector vstart[], int nvar, double x1, double x2, int nstep, void 
 		H.y += Hanis.y;
 		H.z += Hanis.z;
 
+		#if USE_THERMAL
 		H.x += thermX;
 		H.y += thermY;
 		H.z += thermZ;
+		#endif
 
 		(*derivs)(x, v, dv, nvar);
 		rk4(v,dv,nvar,x,h,vout,derivs);
@@ -89,10 +96,12 @@ void rkdumb(SphVector vstart[], int nvar, double x1, double x2, int nstep, void 
 		H.y -= Hanis.y;
 		H.z -= Hanis.z;
 
+		#if USE_THERMAL
 		//Remove thermal motion
 		H.x -= thermX;
 		H.y -= thermY;
 		H.z -= thermZ;
+		#endif
 	}
 
 	free(dv);
@@ -116,14 +125,14 @@ int main(int argc, char *argv[]) {
 	vstart[0].theta = 0.01;
 	vstart[0].phi = 0;
 
-	Vector Happl = {0.0, 0.0, 2500.0};
+	Vector Happl = {0.0, 0.0, 5000.0};
 
 	//Get the step size for the simulation 
 	if(argc < 2) {
 		printf("Usage: %s [step size]\n", argv[0]);
 		return 0;
 	}
-	endTime = (1e6)*strtof(argv[1], NULL); //In us
+	endTime = (1e9)*strtof(argv[1], NULL); //In ns
 	nstep = (int)ceil(endTime/TIMESTEP);
 
 	//Allocate memory for magnetization vector
@@ -136,7 +145,7 @@ int main(int argc, char *argv[]) {
 
 	
 	bool isDecreasing = true;
-	for(int i = 0; i <= 200; i++) {
+	for(int i = 0; i <= 400; i++) {
 		//Applied field
 		H.x = Happl.x;
 		H.y = Happl.y;
@@ -146,14 +155,13 @@ int main(int argc, char *argv[]) {
 		rkdumb(vstart, nvar, 0.0, endTime, nstep, mDot); 
 
 		fprintf(output, "%f\t%f\n", Happl.z, (y[0][nstep].r)*cos(y[0][nstep].theta));
-		printf("%3.1f%% complete\n", 0.5 * i);
 
 		vstart[0].r = y[0][nstep].r;
 		vstart[0].theta = y[0][nstep].theta;
 		vstart[0].phi = y[0][nstep].phi;
 		
 		//Adjust applied field strength at endTime intervals	
-		if(Happl.z + 2500.0 < 1.0) isDecreasing = false;
+		if(Happl.z + 5000.0 < 1.0) isDecreasing = false;
 		isDecreasing ? (Happl.z -= 50.0) : (Happl.z += 50.0);
 	}
 	//Probably don't really need these
