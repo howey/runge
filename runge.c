bfa05943f45078f708ee4f4c7cba07ff1eef58df
Dylan Howey <howeyman09@gmail.com>
2014-01-27 01:25:45 -0600

    CPU and GPU code do the same thing

--- runge.c
+++ runge.c
@@ -1,36 +1,118 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-#include <stdbool.h>
-#include "vector.h"
 #include "runge.h"
-#include "mars.h"
-
-#define USE_THERMAL 1 
 
 /* Time is in units of ns */
-//TODO: Compute alpha here, not on your calculator
 static const double ALPHA = 0.02; 
 static const double GAMMA = 1.76e-2;
-static const double K = 1e6;
-static const double TIMESTEP = (1e-5);	
+static const double KANIS = 1e6;
+static const double TIMESTEP = (1e-5);
+static const double MSAT = 500;
+
+static const double JEX = 1;
 
 static double *xx;
 static SphVector **y;
-static Vector H;
+static Vector *H;
+static Vector Happl;
 
-//Computes the anisotropy field and writes the result to a Vector H
-void anisotropyH(Vector * Ha, const SphVector * M) {
-	Ha->x = (1/M->r) * -2 * K * cos(M->theta) * sin(M->theta) * cos(M->phi) * cos(M->theta);
-	Ha->y = (1/M->r) * -2 * K * cos(M->theta) * sin(M->theta) * sin(M->phi) * cos(M->theta);
-	Ha->z = (1/M->r) * 2 * K * cos(M->theta) * sin(M->theta) * sin(M->theta);
+//Shamelessly copied from Numerical Recipes
+/*
+Given values for the variables y[1..n] and their derivatives dydx[1..n] known at x , use the
+fourth-order Runge-Kutta method to advance the solution over an interval h and return the
+incremented variables as yout[1..n] , which need not be a distinct array from y . The user
+supplies the routine derivs(x,y,dydx) , which returns derivatives dydx at x .
+*/
+void rk4(SphVector y[], SphVector dydx[], int n, double x, double h, SphVector yout[], void (*derivs)(double, SphVector[], SphVector[], int, Vector[])) {
+	double xh, hh, h6; 
+	SphVector *dym, *dyt, *yt;
+
+	dym = (SphVector *)malloc(sizeof(SphVector) * n);
+	dyt = (SphVector *)malloc(sizeof(SphVector) * n);
+	yt = (SphVector *)malloc(sizeof(SphVector) * n);
+
+	hh = h * 0.5;
+	h6 = h / 6.0;
+	xh = x + hh;
+
+	//First step
+	for (int i = 0; i < n; i++) {
+		//yt[i] = y[i] + hh * dydx[i];
+		yt[i].r = y[i].r + hh * dydx[i].r;
+		yt[i].phi = y[i].phi + hh * dydx[i].phi;
+		yt[i].theta = y[i].theta + hh * dydx[i].theta;
+	}
+	//Second step
+	(*derivs)(xh, yt, dyt, n, H);
+	for (int i = 0; i < n; i++) {
+		//yt[i] = y[i] + hh * dyt[i];
+		yt[i].r = y[i].r + hh * dyt[i].r;
+		yt[i].phi = y[i].phi + hh * dyt[i].phi;
+		yt[i].theta = y[i].theta + hh * dyt[i].theta;
+	}
+	//Third step
+	(*derivs)(xh, yt, dym, n, H);
+	for (int i = 0; i < n; i++) {
+		//yt[i] = y[i] + h * dym[i];
+		//dym[i] += dyt[i];
+		yt[i].r = y[i].r + h * dym[i].r;
+		dym[i].r += dyt[i].r;
+		yt[i].phi = y[i].phi + h * dym[i].phi;
+		dym[i].phi += dyt[i].phi;
+		yt[i].theta = y[i].theta + h * dym[i].theta;
+		dym[i].theta += dyt[i].theta;
+	}
+	//Fourth step
+	(*derivs)(x + h, yt, dyt, n, H);
+	//Accumulate increments with proper weights
+	for (int i = 0; i < n; i++) {
+		//yout[i] = y[i] + h6 * (dydx[i] + dyt[i] + 2.0 * dym[i]);
+		yout[i].r = y[i].r + h6 * (dydx[i].r + dyt[i].r + 2.0 * dym[i].r);
+		yout[i].phi = y[i].phi + h6 * (dydx[i].phi + dyt[i].phi + 2.0 * dym[i].phi);
+		yout[i].theta = y[i].theta + h6 * (dydx[i].theta + dyt[i].theta + 2.0 * dym[i].theta);
+	}
+
+	
+	free(yt);
+	free(dyt);
+	free(dym);
 }
 
-void mDot(double t, SphVector M[], SphVector dMdt[], int n) {
-	for(int i = 0; i < n; i++) {
+//Computes the local applied field for every atom of moment M. The global applied field is passed in as Happl. 
+void computeField(Vector * H, Vector Happl, const SphVector * M, int nvar) {
+	for(int i = 0; i < nvar; i++) {
+		//the applied field
+		H[i].x = Happl.x;
+		H[i].y = Happl.y;
+		H[i].z = Happl.z;
+
+		//the anisotropy field
+		H[i].x += (1/M[i].r) * -2 * KANIS * cos(M[i].theta) * sin(M[i].theta) * cos(M[i].phi) * cos(M[i].theta);
+		H[i].y += (1/M[i].r) * -2 * KANIS * cos(M[i].theta) * sin(M[i].theta) * sin(M[i].phi) * cos(M[i].theta);
+		H[i].z += (1/M[i].r) * 2 * KANIS * cos(M[i].theta) * sin(M[i].theta) * sin(M[i].theta);
+
+		//the field from random thermal motion
+		//TODO: sd doesn't have to be computed each time, it is constant
+		#if USE_THERMAL
+		double sd = 3.4e-4/sqrt(TIMESTEP * 1e-9);
+		double thermX = gaussian(0, sd); 
+		double thermY = gaussian(0, sd); 
+		double thermZ = gaussian(0, sd); 
+
+		H[i].x += thermX;
+		H[i].y += thermY;
+		H[i].z += thermZ;
+		#endif
+
+		//TODO: the exchange field
+	}
+}
+
+void mDot(double t, SphVector M[], SphVector dMdt[], int nvar, Vector H[]) {
+
+	//Compute derivative
+	for(int i = 0; i < nvar; i++) {
 		dMdt[i].r = 0;
-		dMdt[i].phi = GAMMA * ((cos(M[i].theta) * sin(M[i].phi) * H.y) / sin(M[i].theta) + (cos(M[i].theta) * cos(M[i].phi) * H.x) / sin(M[i].theta) - H.z) + ((ALPHA * GAMMA)/(1 + ALPHA * ALPHA)) * ((cos(M[i].phi) * H.y) / sin(M[i].theta) - (sin(M[i].phi) * H.x) / sin(M[i].theta));
-		dMdt[i].theta = -GAMMA * (cos(M[i].phi) * H.y - sin(M[i].phi) * H.x) + ((ALPHA * GAMMA)/(1 + ALPHA * ALPHA)) * (cos(M[i].theta) * cos(M[i].phi) * H.x - H.z * sin(M[i].theta) + cos(M[i].theta) * sin(M[i].phi) * H.y);
+		dMdt[i].phi = GAMMA * ((cos(M[i].theta) * sin(M[i].phi) * H[i].y) / sin(M[i].theta) + (cos(M[i].theta) * cos(M[i].phi) * H[i].x) / sin(M[i].theta) - H[i].z) + ((ALPHA * GAMMA)/(1 + ALPHA * ALPHA)) * ((cos(M[i].phi) * H[i].y) / sin(M[i].theta) - (sin(M[i].phi) * H[i].x) / sin(M[i].theta));
+		dMdt[i].theta = -GAMMA * (cos(M[i].phi) * H[i].y - sin(M[i].phi) * H[i].x) + ((ALPHA * GAMMA)/(1 + ALPHA * ALPHA)) * (cos(M[i].theta) * cos(M[i].phi) * H[i].x - H[i].z * sin(M[i].theta) + cos(M[i].theta) * sin(M[i].phi) * H[i].y);
 	}
 }
 
@@ -40,14 +122,14 @@ to advance nstep equal increments to x2. The user-supplied routine derivs(x,v,dv
 evaluates derivatives. Results are stored in the global variables y[0..nvar-1][0..nstep]
 and xx[0..nstep].
 */
-void rkdumb(SphVector vstart[], int nvar, double x1, double x2, int nstep, void (*derivs)(double, SphVector[], SphVector[], int)) {
+void rkdumb(SphVector vstart[], int nvar, double x1, double x2, int nstep, void (*derivs)(double, SphVector[], SphVector[], int, Vector[])) {
 	double x, h;
 	SphVector *v, *vout, *dv;
-	Vector Hanis = {0.0, 0.0, 0.0};
 
 	v = (SphVector *)malloc(sizeof(SphVector) * nvar);
 	vout = (SphVector *)malloc(sizeof(SphVector) * nvar);
 	dv = (SphVector *)malloc(sizeof(SphVector) * nvar);
+	H = (Vector *)malloc(sizeof(Vector) * nvar);
 
 	for (int i = 0;i < nvar;i++) { 
 		v[i] = vstart[i];
@@ -58,29 +140,14 @@ void rkdumb(SphVector vstart[], int nvar, double x1, double x2, int nstep, void 
 	x = x1;
 	h = (x2-x1)/nstep;
 
-	double sd = 3.4e-4/sqrt(TIMESTEP * 1e-9);
 	for (int k = 0; k < nstep; k++) {
 
-		// Add in thermal motion
-		#if USE_THERMAL 
-		double thermX = gaussian(0, sd);
-		double thermY = gaussian(0, sd);
-		double thermZ = gaussian(0, sd);
-		#endif
+		//Compute H field
+		computeField(H, Happl, v, nvar);	
 
-		//Add in anisotropy
-		anisotropyH(&Hanis, &y[0][k]);
-		H.x += Hanis.x;
-		H.y += Hanis.y;
-		H.z += Hanis.z;
-
-		#if USE_THERMAL
-		H.x += thermX;
-		H.y += thermY;
-		H.z += thermZ;
-		#endif
-
-		(*derivs)(x, v, dv, nvar);
+		//Compute derivatives
+		(*derivs)(x, v, dv, nvar, H);
+		
 		rk4(v,dv,nvar,x,h,vout,derivs);
 		if ((double)(x + h) == x) fprintf(stderr, "Step size too small in routine rkdumb");
 		x += h;
@@ -89,31 +156,19 @@ void rkdumb(SphVector vstart[], int nvar, double x1, double x2, int nstep, void 
 			v[i] = vout[i];
 			y[i][k + 1] = v[i];
 		}
-
-		//Remove anisotropy for next step 
-		anisotropyH(&Hanis, &y[0][k]);
-		H.x -= Hanis.x;
-		H.y -= Hanis.y;
-		H.z -= Hanis.z;
-
-		#if USE_THERMAL
-		//Remove thermal motion
-		H.x -= thermX;
-		H.y -= thermY;
-		H.z -= thermZ;
-		#endif
 	}
 
+	free(H);
 	free(dv);
 	free(vout);
 	free(v);
 }
 
 int main(int argc, char *argv[]) {
-	int nvar = 2; //t, M
+	int nvar = HEIGHT * WIDTH * DEPTH; //M for each particle 
 	int nstep;
 	double endTime;
-	SphVector vstart[1]; 
+	SphVector vstart[nvar]; 
 	FILE * output = fopen("output.txt", "w");
 
 	if(output == NULL) {
@@ -121,21 +176,34 @@ int main(int argc, char *argv[]) {
 		return 0;
 	}
 	
-	vstart[0].r = 500;
-	vstart[0].theta = 0.01;
-	vstart[0].phi = 0;
+	//seed random number generator
+	srand(time(NULL));
 
-	Vector Happl = {0.0, 0.0, 5000.0};
+	for(int i = 0; i < nvar; i++) {	
+		vstart[i].r = MSAT;
+		vstart[i].theta = 0.01;
+		vstart[i].phi = 0;
+	}
+
 
 	//Get the step size for the simulation 
 	if(argc < 2) {
 		printf("Usage: %s [step size]\n", argv[0]);
-		return 0;
+		return 1;
 	}
-	endTime = (1e9)*strtof(argv[1], NULL); //In ns
-	nstep = (int)ceil(endTime/TIMESTEP);
 
-	//Allocate memory for magnetization vector
+	//TODO: there has to be a better way of handling input
+	endTime = (1e9)*strtof(argv[1], NULL); //In ns
+	nstep = ((int)ceil(endTime/TIMESTEP));
+
+	nstep = ((nstep + 500) / 1000) * 1000; //round nstep to the nearest thousand
+
+	if((nstep % 1000) == 0) nstep /= 1000; //Divide by 1000 to reduce host memory usage
+	else {
+		fprintf(stderr, "Error: please select step size such that nstep is a multiple of 1000\n");
+		return 1;
+	}
+
 	xx = (double *)malloc(sizeof(double) * (nstep + 1));
 	//TODO: Address y row-major
 	y = (SphVector **)malloc(sizeof(SphVector *) * nvar); 
@@ -143,28 +211,33 @@ int main(int argc, char *argv[]) {
 		y[i] = (SphVector *)malloc(sizeof(SphVector) * (nstep + 1));
 	}
 
-	
 	bool isDecreasing = true;
+	Happl.x = 20.0;
+	Happl.y = 20.0;
+	Happl.z = 5000.0;
+
 	for(int i = 0; i <= 400; i++) {
-		//Applied field
-		H.x = Happl.x;
-		H.y = Happl.y;
-		H.z = Happl.z;
-		
-		//Simulate!
-		rkdumb(vstart, nvar, 0.0, endTime, nstep, mDot); 
+		for(int j = 0; j < 1000; j++) {
+			//Simulate!
+			rkdumb(vstart, nvar, j * endTime / 1000, j* endTime / 1000 + endTime - endTime/nstep, nstep, mDot); 
 
-		fprintf(output, "%f\t%f\n", Happl.z, (y[0][nstep].r)*cos(y[0][nstep].theta));
+			for(int i = 0; i < nvar; i++) {
+				vstart[i].r = y[i][nstep].r;
+				vstart[i].theta = y[i][nstep].theta;
+				vstart[i].phi = y[i][nstep].phi;
+			}
+		}
 
-		vstart[0].r = y[0][nstep].r;
-		vstart[0].theta = y[0][nstep].theta;
-		vstart[0].phi = y[0][nstep].phi;
-		
+		for(int k = 0; k < nvar; k++) {
+			fprintf(output, "%f\t%f\n", Happl.z, (y[k][nstep].r)*cos(y[k][nstep].theta));
+		}
+
 		//Adjust applied field strength at endTime intervals	
 		if(Happl.z + 5000.0 < 1.0) isDecreasing = false;
 		isDecreasing ? (Happl.z -= 50.0) : (Happl.z += 50.0);
 	}
-	//Probably don't really need these
+	
+	//Probably don't really need these since we're about to exit the program
 	free(xx);
 	free(y);
 	return 0;
--- runge.h
+++ runge.h
@@ -1,9 +1,37 @@
-#define USE_THERMAL 1 //Simulate random thermal motion. 1 enables, 0 disables 
+#define USE_THERMAL 0 //Simulate random thermal motion. 1 enables, 0 disables 
 
-#define HEIGHT 4 //Atoms in y direction
-#define WIDTH 4 //Atoms in x direction
-#define DEPTH 4 //Atoms in z direction
+#define HEIGHT 2 //Atoms in y direction
+#define WIDTH 2 //Atoms in x direction
+#define DEPTH 2 //Atoms in z direction
 
 #define BLOCK_SIZE 4 //Side length of CUDA block
 
-__global__ void mDot(double, SphVector[], SphVector[], int, Vector[]);
+#define gaussian(mean, sd) ((mean) + (sd) * nextGaussian())
+
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stdbool.h>
+
+//Type declarations
+
+//A rectangular vector structure
+typedef struct {
+	double x;
+	double y;
+	double z;
+} Vector;
+
+//A spherical vector structure
+typedef struct {
+	double r;
+	double theta;
+	double phi;
+} SphVector;
+
+//Function prototypes
+
+double nextDouble(double, double);
+double nextGaussian();
+void mDot(double, SphVector[], SphVector[], int, Vector[]);
